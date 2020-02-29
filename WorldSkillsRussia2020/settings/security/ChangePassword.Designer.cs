1fae085e18d28f45858cebd941702f5000281fd8
rrm1 <rustamramazanovworkmail@gmail.com>
2020-02-29 03:38:40 +0300

    Добавлено: форма изменения пароля.

--- WorldSkillsRussia2020/settings/security/ChangePassword.Designer.cs
+++ WorldSkillsRussia2020/settings/security/ChangePassword.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace WorldSkillsRussia2020.handlers
+﻿namespace WorldSkillsRussia2020.settings.security
 {
-    partial class ChangePass
+    partial class ChangePassword
     {
         /// <summary>
         /// Required designer variable.
@@ -28,54 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.oldPass = new System.Windows.Forms.TextBox();
             this.bg_input = new System.Windows.Forms.Panel();
+            this.confPassImg = new System.Windows.Forms.PictureBox();
             this.confPass = new System.Windows.Forms.TextBox();
             this.changePassBtn = new System.Windows.Forms.Button();
             this.descAttempt = new System.Windows.Forms.Label();
+            this.oldPassImg = new System.Windows.Forms.PictureBox();
             this.newPass = new System.Windows.Forms.TextBox();
-            this.oldPass = new System.Windows.Forms.TextBox();
+            this.newPassImg = new System.Windows.Forms.PictureBox();
             this.bg_header = new System.Windows.Forms.Panel();
-            this.signInWinState = new System.Windows.Forms.Label();
+            this._collapse = new System.Windows.Forms.Label();
             this.xExit = new System.Windows.Forms.Label();
             this.header_text = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pass_img = new System.Windows.Forms.PictureBox();
+            this.oldPassToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.newPassToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.confPassToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.collToolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.exitToolTip = new System.Windows.Forms.ToolTip(this.components);
             this.bg_input.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.confPassImg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oldPassImg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.newPassImg)).BeginInit();
             this.bg_header.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pass_img)).BeginInit();
             this.SuspendLayout();
+            // 
+            // oldPass
+            // 
+            this.oldPass.Font = new System.Drawing.Font("Verdana", 14F);
+            this.oldPass.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.oldPass.Location = new System.Drawing.Point(126, 127);
+            this.oldPass.Multiline = true;
+            this.oldPass.Name = "oldPass";
+            this.oldPass.Size = new System.Drawing.Size(399, 64);
+            this.oldPass.TabIndex = 2;
             // 
             // bg_input
             // 
             this.bg_input.BackColor = System.Drawing.Color.Azure;
-            this.bg_input.Controls.Add(this.pictureBox1);
+            this.bg_input.Controls.Add(this.confPassImg);
             this.bg_input.Controls.Add(this.confPass);
             this.bg_input.Controls.Add(this.changePassBtn);
             this.bg_input.Controls.Add(this.descAttempt);
-            this.bg_input.Controls.Add(this.pictureBox2);
+            this.bg_input.Controls.Add(this.oldPassImg);
             this.bg_input.Controls.Add(this.newPass);
-            this.bg_input.Controls.Add(this.pass_img);
+            this.bg_input.Controls.Add(this.newPassImg);
             this.bg_input.Controls.Add(this.oldPass);
             this.bg_input.Controls.Add(this.bg_header);
             this.bg_input.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.bg_input.Dock = System.Windows.Forms.DockStyle.Fill;
             this.bg_input.Location = new System.Drawing.Point(0, 0);
             this.bg_input.Name = "bg_input";
-            this.bg_input.Size = new System.Drawing.Size(600, 500);
+            this.bg_input.Size = new System.Drawing.Size(600, 520);
             this.bg_input.TabIndex = 1;
+            // 
+            // confPassImg
+            // 
+            this.confPassImg.Image = global::WorldSkillsRussia2020.Properties.Resources.icons8_password_100__1_;
+            this.confPassImg.Location = new System.Drawing.Point(36, 322);
+            this.confPassImg.Name = "confPassImg";
+            this.confPassImg.Size = new System.Drawing.Size(64, 64);
+            this.confPassImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.confPassImg.TabIndex = 12;
+            this.confPassImg.TabStop = false;
             // 
             // confPass
             // 
             this.confPass.Font = new System.Drawing.Font("Verdana", 14F);
             this.confPass.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.confPass.Location = new System.Drawing.Point(126, 318);
+            this.confPass.Location = new System.Drawing.Point(126, 322);
             this.confPass.Multiline = true;
             this.confPass.Name = "confPass";
             this.confPass.Size = new System.Drawing.Size(399, 64);
-            this.confPass.TabIndex = 13;
+            this.confPass.TabIndex = 11;
             this.confPass.UseSystemPasswordChar = true;
             // 
             // changePassBtn
@@ -89,7 +115,7 @@
             this.changePassBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.changePassBtn.Font = new System.Drawing.Font("Lucida Console", 12F);
             this.changePassBtn.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.changePassBtn.Location = new System.Drawing.Point(221, 440);
+            this.changePassBtn.Location = new System.Drawing.Point(219, 449);
             this.changePassBtn.Name = "changePassBtn";
             this.changePassBtn.Size = new System.Drawing.Size(159, 48);
             this.changePassBtn.TabIndex = 5;
@@ -102,10 +128,20 @@
             this.descAttempt.BackColor = System.Drawing.Color.SeaGreen;
             this.descAttempt.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.descAttempt.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.descAttempt.Location = new System.Drawing.Point(38, 392);
+            this.descAttempt.Location = new System.Drawing.Point(64, 403);
             this.descAttempt.Name = "descAttempt";
             this.descAttempt.Size = new System.Drawing.Size(0, 25);
             this.descAttempt.TabIndex = 10;
+            // 
+            // oldPassImg
+            // 
+            this.oldPassImg.Image = global::WorldSkillsRussia2020.Properties.Resources.icons8_password_100;
+            this.oldPassImg.Location = new System.Drawing.Point(36, 127);
+            this.oldPassImg.Name = "oldPassImg";
+            this.oldPassImg.Size = new System.Drawing.Size(64, 64);
+            this.oldPassImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.oldPassImg.TabIndex = 9;
+            this.oldPassImg.TabStop = false;
             // 
             // newPass
             // 
@@ -118,21 +154,21 @@
             this.newPass.TabIndex = 4;
             this.newPass.UseSystemPasswordChar = true;
             // 
-            // oldPass
+            // newPassImg
             // 
-            this.oldPass.Font = new System.Drawing.Font("Verdana", 14F);
-            this.oldPass.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.oldPass.Location = new System.Drawing.Point(126, 127);
-            this.oldPass.Multiline = true;
-            this.oldPass.Name = "oldPass";
-            this.oldPass.Size = new System.Drawing.Size(399, 64);
-            this.oldPass.TabIndex = 2;
+            this.newPassImg.Image = global::WorldSkillsRussia2020.Properties.Resources.icons8_password_100__1_;
+            this.newPassImg.Location = new System.Drawing.Point(35, 226);
+            this.newPassImg.Name = "newPassImg";
+            this.newPassImg.Size = new System.Drawing.Size(64, 64);
+            this.newPassImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.newPassImg.TabIndex = 3;
+            this.newPassImg.TabStop = false;
             // 
             // bg_header
             // 
             this.bg_header.AutoSize = true;
             this.bg_header.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.bg_header.Controls.Add(this.signInWinState);
+            this.bg_header.Controls.Add(this._collapse);
             this.bg_header.Controls.Add(this.xExit);
             this.bg_header.Controls.Add(this.header_text);
             this.bg_header.Cursor = System.Windows.Forms.Cursors.Hand;
@@ -142,17 +178,17 @@
             this.bg_header.Size = new System.Drawing.Size(600, 60);
             this.bg_header.TabIndex = 0;
             // 
-            // signInWinState
+            // _collapse
             // 
-            this.signInWinState.AutoSize = true;
-            this.signInWinState.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.signInWinState.Font = new System.Drawing.Font("Lucida Console", 16F);
-            this.signInWinState.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.signInWinState.Location = new System.Drawing.Point(526, 9);
-            this.signInWinState.Name = "signInWinState";
-            this.signInWinState.Size = new System.Drawing.Size(28, 27);
-            this.signInWinState.TabIndex = 2;
-            this.signInWinState.Text = "-";
+            this._collapse.AutoSize = true;
+            this._collapse.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this._collapse.Font = new System.Drawing.Font("Lucida Console", 16F);
+            this._collapse.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this._collapse.Location = new System.Drawing.Point(526, 9);
+            this._collapse.Name = "_collapse";
+            this._collapse.Size = new System.Drawing.Size(28, 27);
+            this._collapse.TabIndex = 2;
+            this._collapse.Text = "-";
             // 
             // xExit
             // 
@@ -179,73 +215,46 @@
             this.header_text.Text = "Изменить пароль";
             this.header_text.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::WorldSkillsRussia2020.Properties.Resources.icons8_password_100__1_;
-            this.pictureBox1.Location = new System.Drawing.Point(36, 318);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 14;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Image = global::WorldSkillsRussia2020.Properties.Resources.icons8_password_100;
-            this.pictureBox2.Location = new System.Drawing.Point(36, 127);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 9;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pass_img
-            // 
-            this.pass_img.Image = global::WorldSkillsRussia2020.Properties.Resources.icons8_password_100__1_;
-            this.pass_img.Location = new System.Drawing.Point(35, 226);
-            this.pass_img.Name = "pass_img";
-            this.pass_img.Size = new System.Drawing.Size(64, 64);
-            this.pass_img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pass_img.TabIndex = 3;
-            this.pass_img.TabStop = false;
-            // 
-            // ChangePass
+            // ChangePassword
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 500);
+            this.ClientSize = new System.Drawing.Size(600, 520);
             this.Controls.Add(this.bg_input);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MaximumSize = new System.Drawing.Size(600, 500);
-            this.MinimumSize = new System.Drawing.Size(600, 500);
-            this.Name = "ChangePass";
+            this.Name = "ChangePassword";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ChangePass";
+            this.Text = "ChangePassword";
             this.bg_input.ResumeLayout(false);
             this.bg_input.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.confPassImg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oldPassImg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.newPassImg)).EndInit();
             this.bg_header.ResumeLayout(false);
             this.bg_header.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pass_img)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TextBox oldPass;
         private System.Windows.Forms.Panel bg_input;
         private System.Windows.Forms.Button changePassBtn;
         private System.Windows.Forms.Label descAttempt;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox oldPassImg;
         private System.Windows.Forms.TextBox newPass;
-        private System.Windows.Forms.PictureBox pass_img;
-        private System.Windows.Forms.TextBox oldPass;
+        private System.Windows.Forms.PictureBox newPassImg;
         private System.Windows.Forms.Panel bg_header;
-        private System.Windows.Forms.Label signInWinState;
+        private System.Windows.Forms.Label _collapse;
         private System.Windows.Forms.Label xExit;
         private System.Windows.Forms.Label header_text;
         private System.Windows.Forms.TextBox confPass;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox confPassImg;
+        private System.Windows.Forms.ToolTip oldPassToolTip;
+        private System.Windows.Forms.ToolTip newPassToolTip;
+        private System.Windows.Forms.ToolTip confPassToolTip;
+        private System.Windows.Forms.ToolTip collToolTip;
+        private System.Windows.Forms.ToolTip exitToolTip;
     }
 }
---
+++ WorldSkillsRussia2020/settings/security/ChangePassword.cs
@@ -0,0 +1,129 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Data;
+using WorldSkillsRussia2020.handlers;
+using System.Threading.Tasks;
+
+namespace WorldSkillsRussia2020.settings.security
+{
+    internal partial class ChangePassword : Form
+    {
+        // Иниц. переменной позиции окна.
+        Point lastpoint;
+        internal ChangePassword()
+        {
+            InitializeComponent();
+
+
+            // ToolTip's.
+            _collapse.MouseHover += (s, a) => { collToolTip.SetToolTip(_collapse, "Свернуть"); };
+            xExit.MouseHover += (s, a) => { exitToolTip.SetToolTip(xExit, "Закрыть"); };
+
+            oldPass.MouseHover += (s, a) => { oldPassToolTip.SetToolTip(oldPass, "Старый пароль"); };
+            newPass.MouseHover += (s, a) => { newPassToolTip.SetToolTip(newPass, "Новый пароль"); };
+            confPass.MouseHover += (s, a) => { confPassToolTip.SetToolTip(confPass, "Повторите пароль"); };
+
+            // Кнопка Свернуть.
+            _collapse.Click += (s, e) => { WindowState = FormWindowState.Minimized; };
+            // Замена цвета кнопки Свернуть.
+            _collapse.MouseEnter += (s, a) => { _collapse.BackColor = Color.MediumSpringGreen; };
+            _collapse.MouseLeave += (s, a) => { _collapse.BackColor = Color.MediumSeaGreen; };
+
+            // Кнопка Закрыть.
+            xExit.Click += (s, a) => { Dispose(); };
+            // Замена цвета кнопки Закрыть.
+            xExit.MouseEnter += (s, a) => { xExit.BackColor = Color.Red; };
+            xExit.MouseLeave += (s, a) => { xExit.BackColor = Color.MediumSeaGreen; };
+
+            // Отключение перехода по нажатию клавиши Enter в textBox'ах.
+            // Старый пароль.
+            oldPass.KeyDown += (s, a) =>
+            {
+                if (a.KeyCode == Keys.Enter)
+                    a.SuppressKeyPress = true;
+            };
+            // Новый пароль.
+            newPass.KeyDown += (s, a) =>
+            {
+                if (a.KeyCode == Keys.Enter)
+                    a.SuppressKeyPress = true;
+            };
+            // Подтвердите пароль.
+            confPass.KeyDown += (s, a) =>
+            {
+                if (a.KeyCode == Keys.Enter)
+                    a.SuppressKeyPress = true;
+            };
+
+            // Перетаскивание окна по ЛКМ.
+            header_text.MouseMove += (s, a) =>
+            {
+                if (a.Button == MouseButtons.Left)
+                {
+                    Left += a.X - lastpoint.X;
+                    Top += a.Y - lastpoint.Y;
+                }
+            };
+            // Новые координаты для окна.
+            header_text.MouseDown += (s, a) => { lastpoint = new Point(a.X, a.Y); };
+
+            // Событие Enter для кнопки Изменить.
+            KeyUp += (s, a) =>
+            {
+                if (a.KeyCode == Keys.Enter)
+                    changePassBtn.PerformClick();
+            };
+
+            // Замена цвета при наведении у кнопки Изменить.
+            changePassBtn.MouseEnter += (s, a) =>
+            {
+                changePassBtn.BackColor = Color.SeaGreen;
+                changePassBtn.ForeColor = Color.MistyRose;
+            };
+            changePassBtn.MouseLeave += (s, a) =>
+            {
+                changePassBtn.BackColor = Color.LightGray;
+                changePassBtn.ForeColor = Color.DarkSlateGray;
+            };
+
+            // Кнопка Изменить.
+            changePassBtn.Click += async (s, a) =>
+            {
+               
+                // Переменные полей.
+                string oldpass = oldPass.Text;
+                string newpass = newPass.Text;
+                string confpass = confPass.Text;
+
+                var db = new DB();
+                var sda = new SqlDataAdapter();
+                var dt = new DataTable();
+
+                // Валидация полей.
+                if (oldpass == "" || newpass == "" || confpass == "")
+                {
+                    descAttempt.Text = "Одно или более полей не заполнены!";
+                    await Task.Delay(4000);
+                    descAttempt.Text = "";
+                }
+                if (newpass != confpass)
+                {
+                    descAttempt.Text = "Последние 2 поля не совпадают!";
+                    await Task.Delay(4000);
+                    descAttempt.Text = "";
+                }
+                if (oldpass == newpass || oldpass == confpass)
+                {
+                    descAttempt.Text = "Старый пароль и новый совпадают!";
+                    await Task.Delay(4000);
+                    descAttempt.Text = "";
+                }
+
+                //Dispose();
+                //var signin = new Signin();
+                //signin.Show();
+            };
+        }
+    }
+}
